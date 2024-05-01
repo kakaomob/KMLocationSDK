ebe264b729fc77efd78c0d5966d1ce4e43b1bde0
Steve Ham <steve.ham@kakaomobility.com>
2024-05-01 11:11:19 +0900

    removed flatbuffers in right way.

--- Package.swift
+++ Package.swift
@@ -21,13 +21,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tjlabs/jupiter-sdk-spm", exact: Version(stringLiteral: "3.4.3"))
+        .package(url: "https://github.com/tjlabs/jupiter-sdk-spm", exact: Version(stringLiteral: "3.4.3")),
+//        .package(url: "https://github.com/google/flatbuffers.git", exact: Version(stringLiteral: "22.10.26")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: sdkName, url: binaryUrl, checksum: checksum),
         .target(name: "\(sdkName)Wrapper",
-                dependencies: [.target(name: sdkName), .product(name: "JupiterSDK", package: "jupiter-sdk-spm")])
+                dependencies: [.target(name: sdkName),
+                    .product(name: "JupiterSDK", package: "jupiter-sdk-spm")
+                    /*.product(name: "FlatBuffers", package: "flatbuffers")*/])
     ]
 )
