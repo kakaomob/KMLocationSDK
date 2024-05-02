9a0d387583cee268fe0d49cef2ac4d0a1f4c1125
Steve Ham <steve.ham@kakaomobility.com>
2024-05-02 14:43:02 +0900

    1.103.82

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let sdkName = "KMLocationSDK"
 
-let binaryUrl = "https://devrepo.kakaomobility.com/repository/LocationCocoaPod/locationsdk-ios/1.3.81/KMLocationSDK.xcframework.zip"
-let checksum = "12b71d66758600eebe34dce25fcd6bec38827802f3c31565dc6af5d72edf944e"
+let binaryUrl = "https://devrepo.kakaomobility.com/repository/LocationCocoaPod/locationsdk-ios/1.103.82/KMLocationSDK.xcframework.zip"
+let checksum = "18d1cbd6681bcf8858196c4f727f54841804b686d805b348cd78bc4cd861c8f3"
 
 let package = Package(
     name: sdkName,
@@ -21,16 +21,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tjlabs/jupiter-sdk-spm", exact: Version(stringLiteral: "3.4.3")),
-//        .package(url: "https://github.com/google/flatbuffers.git", exact: Version(stringLiteral: "22.10.26")),
+//        .package(url: "https://github.com/tjlabs/jupiter-sdk-spm", exact: Version(stringLiteral: "3.4.3")),
+//        .package(url: "https://github.com/tjlabs/jupiter-sdk-spm", exact: Version(stringLiteral: "3.4.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: sdkName, url: binaryUrl, checksum: checksum),
         .target(name: "\(sdkName)Wrapper",
-                dependencies: [.target(name: sdkName),
-                    .product(name: "JupiterSDK", package: "jupiter-sdk-spm")
-                    /*.product(name: "FlatBuffers", package: "flatbuffers")*/])
+                dependencies: [.target(name: sdkName)])
+//                    .product(name: "JupiterSDK", package: "jupiter-sdk-spm")])
+                    /*.product(name: "FlatBuffers", package: "flatbuffers")*///])
     ]
 )
