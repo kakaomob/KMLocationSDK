a9bbe0312401c10ff1af3db3b4413ac0923d432d
Steve Ham <steve.ham@kakaomobility.com>
2024-05-02 23:06:11 +0900

    1.103.82

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let sdkName = "KMLocationSDK"
 
-let binaryUrl = "https://devrepo.kakaomobility.com/repository/LocationCocoaPod/locationsdk-ios/1.3.82/KMLocationSDK.xcframework.zip"
-let checksum = "0130aa218cc17b912477ae1695413f0bd8858f2d4f21564aa9a5c5d5c8f10b2f"
+let binaryUrl = "https://devrepo.kakaomobility.com/repository/LocationCocoaPod/locationsdk-ios/1.103.82/KMLocationSDK.xcframework.zip"
+let checksum = "683110b297684dc3ffae06673982c4841924a8663e151c85229c754fd91b09b4"
 
 let package = Package(
     name: sdkName,
@@ -21,16 +21,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tjlabs/jupiter-sdk-spm", exact: Version(stringLiteral: "3.4.3"))
 //        .package(url: "https://github.com/tjlabs/jupiter-sdk-spm", exact: Version(stringLiteral: "3.4.3")),
+//        .package(url: "https://github.com/google/flatbuffers.git", exact: Version(stringLiteral: "22.10.26")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: sdkName, url: binaryUrl, checksum: checksum),
         .target(name: "\(sdkName)Wrapper",
-                dependencies: [.target(name: sdkName), .product(name: "JupiterSDK", package: "jupiter-sdk-spm")])
-//                    .product(name: "JupiterSDK", package: "jupiter-sdk-spm")])
-                    /*.product(name: "FlatBuffers", package: "flatbuffers")*///])
+                dependencies: [.target(name: sdkName),
+//                    .product(name: "JupiterSDK", package: "jupiter-sdk-spm")
+                    /*.product(name: "FlatBuffers", package: "flatbuffers")*/])
     ]
 )
