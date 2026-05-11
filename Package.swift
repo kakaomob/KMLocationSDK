3586a94949f96e6280de1c0a262a4b1c0f82c9f2
steve.ham <steve.ham@kakaomobility.com>
2026-05-11 20:43:20 +0900

    1.105.73

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let sdkName = "KMLocationSDK"
 
-let binaryUrl = "https://devrepo.kakaomobility.com/repository/LocationCocoaPod/locationsdk-ios/1.5.72/KMLocationSDK.xcframework.zip"
-let checksum = "2feafa94ee0c341a1fd7d07e5033f797cda467f1a842f3a13cd3d0b412fb9314"
+let binaryUrl = "https://devrepo.kakaomobility.com/repository/LocationCocoaPod/locationsdk-ios/1.105.73/KMLocationSDK.xcframework.zip"
+let checksum = "29e933b10e76ffb7a507e143d435d220b17568dc96393a6a0eec6ed0367eb9bf"
 
 let package = Package(
     name: sdkName,
